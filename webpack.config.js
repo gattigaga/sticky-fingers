241b60775e4d74845a0e79517f8df3c98747f68c
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2020-04-10 19:29:29 +0700

    Install babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,18 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "build"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
 };
