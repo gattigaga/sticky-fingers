907a0f730ff50aa9fcd8d3646613ae63fff4ed40
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2021-01-20 20:30:44 +0700

    Add doc block in helpers file

--- src/scripts/helpers.js
+++ src/scripts/helpers.js
@@ -1,6 +1,13 @@
 import randomWords from "random-words";
 import { compose, map, flatten, find, findIndex, filter } from "lodash/fp";
 
+/**
+ * Get total chars of words.
+ * Example: "hello world" words has total 11 chars.
+ *
+ * @param {Object[]} words Words that contains the chars
+ * @return {Number} Total chars
+ */
 export const getTotalChars = (words) => {
   return compose(
     (chars) => chars.length,
@@ -9,6 +16,13 @@ export const getTotalChars = (words) => {
   )(words);
 };
 
+/**
+ * Get total chars of words that mistyped.
+ * Example: "h*llo wor*d" words has total 2 mistyped chars.
+ *
+ * @param {Object[]} words Words that contains the chars
+ * @return {Number} Total mistyped chars
+ */
 export const getTotalErrors = (words) => {
   return compose(
     (chars) => chars.length,
@@ -18,14 +32,29 @@ export const getTotalErrors = (words) => {
   )(words);
 };
 
-export const getSpeed = (elapsedTimeInMs, words) => {
+/**
+ * Get your typing speed based on how much words you can correctly
+ * type in specified amount of time.
+ *
+ * @param {Number} time Time in milliseconds
+ * @param {Object[]} words Words that contains the chars
+ * @return {Number} Typing speed
+ */
+export const getSpeed = (time, words) => {
   const totalChars = getTotalChars(words);
-  const elapsedTimeInMinutes = elapsedTimeInMs / 60000;
-  const result = totalChars / 5 / elapsedTimeInMinutes;
+  const timeInMinutes = time / 60000;
+  const result = totalChars / 5 / timeInMinutes;
 
   return Number(result.toFixed(1));
 };
 
+/**
+ * Get index of current active char that need to type.
+ * Example: the index of active char in "hello *orld" words is 6.
+ *
+ * @param {Object[]} words Words that contains the chars
+ * @return {Number} Index of active char
+ */
 export const getActiveCharIndex = (words) => {
   return compose(
     findIndex((char) => char.isActive),
@@ -34,6 +63,14 @@ export const getActiveCharIndex = (words) => {
   )(words);
 };
 
+/**
+ * Set index of char that need to be active.
+ * There is only one active char in the words.
+ *
+ * @param {Number} index Index of char that need to be active
+ * @param {Object[]} words Words that contains the chars
+ * @return {Object[]} Updated words with new active char
+ */
 export const setActiveCharIndex = (index, words) => {
   let charIndex = 0;
 
@@ -51,6 +88,14 @@ export const setActiveCharIndex = (index, words) => {
   });
 };
 
+/**
+ * Set index of char that need to be error.
+ * Multiple char errors in the words is possible.
+ *
+ * @param {Number} index Index of char that need to be error
+ * @param {Object[]} words Words that contains the chars
+ * @return {Object[]} Updated words with new error char
+ */
 export const setErrorCharIndex = (index, words) => {
   let charIndex = 0;
 
@@ -67,6 +112,13 @@ export const setErrorCharIndex = (index, words) => {
   });
 };
 
+/**
+ * Generate specified amount of words.
+ *
+ * @param {Number} totalWords How much words should be generated ?
+ * @param {Number} offset Default index of active char
+ * @return {Object[]} Words
+ */
 export const generateWords = (totalWords, offset = 0) => {
   const totalSpaces = totalWords - 1;
   const totalAll = totalSpaces + totalWords;
@@ -110,6 +162,13 @@ export const generateWords = (totalWords, offset = 0) => {
   return results;
 };
 
+/**
+ * Check if currently pressed key match with active char.
+ *
+ * @param {Number} keyCode Code of key that currently pressed
+ * @param {Object[]} words Words that contains the chars
+ * @return {Boolean} Is match ?
+ */
 export const isActiveCharMatch = (keyCode, words) => {
   return compose(
     (char) => keyCode === char.code,
@@ -119,6 +178,12 @@ export const isActiveCharMatch = (keyCode, words) => {
   )(words);
 };
 
+/**
+ * Check if active char is at the last index.
+ *
+ * @param {Object[]} words Words that contains the chars
+ * @return {Boolean} Is at last ?
+ */
 export const isActiveCharLast = (words) => {
   const totalChars = getTotalChars(words);
   const convertedFind = find.convert({ cap: false });
